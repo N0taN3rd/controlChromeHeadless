4a596d59b1f35671e06bae7d439c84f584fa0620
N0taN3rd <n0tan3rd@gmail.com>
2017-06-28 01:39:51 -0400

    add copyrights etc etc

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
+/*
+ Control Chrome Headless  Copyright (C) 2017  John Berlin <n0tan3rd@gmail.com>
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Control Chrome Headless is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this Control Chrome Headless.  If not, see <http://www.gnu.org/licenses/>
+ */
+
 const program = require('commander')
 const R = require('ramda')
 const configRunner = require('./lib/runners').configRunner
 const cp = require('./lib/utils/colorPrinters')
+
+cp.yellow(`
+    Control Chrome Headless  Copyright (C) 2017  John Berlin <n0tan3rd@gmail.com>
+    
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Control Chrome Headless is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this Control Chrome Headless.  If not, see <http://www.gnu.org/licenses/>
+`)
 
 program
   .version('1.0.0', '-v, --version')
